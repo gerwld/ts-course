410a9fa98cf92a0e34ee3a8f84b76f3f380a1034
P Jaworski <pjaworski.dev@gmail.com>
2025-02-16 17:39:03 +0100

    function and tuple type

--- dist/main.js
+++ dist/main.js
@@ -9,3 +9,20 @@ const address = {
     street: 'ewrwer',
 };
 const common = Object.assign(Object.assign({}, address), user);
+let array;
+// let array:Array<string>; same thing but by using generics
+array = ["123", "123123"];
+let numbers = [1, 2, 3, 4,]; // readonly array with number generic
+const newArray = [1, "2", null];
+function getChannelFunc(name = "something-arg") {
+    return { name };
+}
+// less convinient
+const getChannelArrow = (name = "something-arg") => {
+    return { name };
+};
+// more convinient
+const getChannelArrowSameShitButBetter = (name) => {
+    return { name };
+};
+// getChannel(3);
--- src/main.ts
+++ src/main.ts
@@ -21,3 +21,36 @@ const address: TypeAddress = {
 };
 
 const common: TypeAddress & TypeUser = { ...address, ...user };
+
+
+let array:string[];
+// let array:Array<string>; same thing but by using generics
+
+array = ["123", "123123"]
+
+let numbers:ReadonlyArray<number> = [1,2,3,4,] // readonly array with number generic
+
+// numbers[0] = "3";
+
+type TypeArray = [number, string, null]
+const newArray:TypeArray = [1, "2", null]
+
+type TypeChannelReturn = {
+   name: string
+}
+
+function getChannelFunc(name:string = "something-arg"):TypeChannelReturn {
+   return {name};
+}
+
+type TypeChannelFunction = (name: string) => TypeChannelReturn;
+// less convinient
+const getChannelArrow = (name:string = "something-arg"):TypeChannelReturn => {
+   return {name};
+}
+// more convinient
+const getChannelArrowSameShitButBetter:TypeChannelFunction = (name) => {
+   return {name};
+}
+
+// getChannel(3);
