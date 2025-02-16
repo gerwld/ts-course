dbcbf2d639f562fc3c3bf9dfec3eb96ba1781e7a
P Jaworski <pjaworski.dev@gmail.com>
2025-02-16 17:24:08 +0100

    Init

---
+++ dist/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const user = {
+    address: '123',
+    age: 12,
+    name: 'sedfw',
+};
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+type TypeUser = {
+   name: string;
+   age: number;
+   address: string;
+};
+
+const user: TypeUser = {
+   address: '123',
+   age: 12,
+   name: 'sedfw',
+};
